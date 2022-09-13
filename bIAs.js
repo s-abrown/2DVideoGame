d9220601e79800eab6e302958f4c9ca094acb025
tessa-cat <82821292+tessa-cat@users.noreply.github.com>
2022-09-13 11:38:43 +0200

    worked on intro scene

--- bIAs.js
+++ bIAs.js
@@ -5,12 +5,28 @@ kaboom({
     height: 800,
 });
 
-///////////// SCENE ONE: INTRODUCTIONS /////////////
+layers([
+	"game",
+	"ui",
+    "game"]),
 
-scene("intro", () =>{
+///////////// ASSETS /////////////
+
+loadRoot("assets/")
+
+loadSprite("classRoom1","classroom.jpeg")
+
+loadBean("bean")
+
+///////////// SCENE ONE: Start Screen /////////////
+
+scene("accueil", () =>{
     add([
-        text(`THIS IS THE MOST AWESOME GAME 
-YOU'LL EVER GET TO PLAY, ENJOY`, {
+        text(`bIAs
+        
+PRESS SPACE BAR TO BEGIN
+
+A game brought to you by Sophie and Tessa`, {
             size: 48,
             font: "sink",
         }),
@@ -19,14 +35,104 @@ YOU'LL EVER GET TO PLAY, ENJOY`, {
 
     // Ajouter un évennement quand j'appuie sur une touche
     onKeyPress("space", () =>{
-        go("dialogue");
+        go("introduction");
     });
 });
 
-///////////// SCENE TWO: DIALOGUE /////////////
-// Introduction to the first challange: choosing a teacher for your new school
-scene("dialogue", () =>{
-});
+///////////// SCENE TWO: Introduction /////////////
+// Your teacher introduces the AI, K.A.T.E. to the class and contextualises the game.
+
+// Adding the background image of the scene: 
+scene("introduction", () =>{
+    let class1 = add([
+        sprite("classRoom1"),
+        // Make the background centered on the screen
+        pos(width() / 2, height() / 2),
+        origin("center"),
+        // Allow the background to be scaled
+        //scale(1),
+        // Keep the background position fixed even when the camera moves
+        fixed()
+      ]);
+
+    // Adding the dialogue/text box at the bottom of the screen:
+    const textbox = add([
+        rect(width() - 300, 220, { radius: 32 }),
+        origin("center"),
+        pos(center().x + 100, height() - 125),
+        outline(2),
+    ]);
+
+    const portrait = add([
+        rect(200, 220, {radius: 32}),
+        origin("center"),
+        pos(center().x - 450, height() - 125),
+        outline(2),
+    ]);
+
+    const dialogs = [
+        [ "bean", "hi my buttefly" ],
+        [ "bean", "i love u" ],
+        [ "bean", "you love me? pretty baby" ],
+        [ "bean", "mark is a stupid" ],
+        [ "bean", "he did not know how to take care of you..." ],
+        [ "mark", "you don't know me ..." ],
+        [ "bean", "what! mark???" ],
+        [ "mark", "oh...hi " ],
+    ];
+
+    let curDialog = 0
+
+    // Text
+    const txt = add([
+	text("", { size: 32, width: width() - 230 }),
+	pos(textbox.pos),
+	origin("center")
+    ]);
+
+    // Character avatar
+    const avatar = add([
+	sprite("bean"),
+	scale(3),
+	origin("center"),
+	pos(portrait.pos),
+    ])
+
+    onKeyPress("space", () => {
+        // Cycle through the dialogs
+        curDialog = (curDialog + 1) % dialogs.length
+        updateDialog()
+    });
+
+    // Update the on screen sprite & text
+    function updateDialog() {
+
+	const [ char, dialog ] = dialogs[curDialog]
+
+	// Use a new sprite component to replace the old one
+	//avatar.use(sprite(char))
+	// Update the dialog text
+	txt.text = dialog
+
+}
+
+updateDialog()
+
+
+
+    
+
+
+
+
+
+
+
+
+    })
+
+
+
 
 // Initialize game 
-go("intro")
+go("introduction")
